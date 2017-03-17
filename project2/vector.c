c5b860fdb06e901021369ec0335e209d7cee84a0
Lauren Yeung <lyeung.ca@gmail.com>
2017-03-16 23:59:00 -0700

    comment out the vector main

--- project2/vector.c
+++ project2/vector.c
@@ -70,32 +70,32 @@ void vector_free(vector *v)
     free(v->data);
 }
 
-int main(void)
-{
-    vector v;
-    vector_init(&v);
+// int main(void)
+// {
+//     vector v;
+//     vector_init(&v);
 
-    vector_add(&v, "emil");
-    vector_add(&v, "hannes");
-    vector_add(&v, "lydia");
-    vector_add(&v, "olle");
-    vector_add(&v, "erik");
+//     vector_add(&v, "emil");
+//     vector_add(&v, "hannes");
+//     vector_add(&v, "lydia");
+//     vector_add(&v, "olle");
+//     vector_add(&v, "erik");
 
-    int i;
-    printf("first round:\n");
-    for (i = 0; i < vector_count(&v); i++) {
-        printf("%s\n", (char*)vector_get(&v, i));
-    }
+//     int i;
+//     printf("first round:\n");
+//     for (i = 0; i < vector_count(&v); i++) {
+//         printf("%s\n", (char*)vector_get(&v, i));
+//     }
 
-    vector_delete(&v, 1);
-    vector_delete(&v, 3);
+//     vector_delete(&v, 1);
+//     vector_delete(&v, 3);
 
-    printf("second round:\n");
-    for (i = 0; i < vector_count(&v); i++) {
-        printf("%s\n", (char*)vector_get(&v, i));
-    }
+//     printf("second round:\n");
+//     for (i = 0; i < vector_count(&v); i++) {
+//         printf("%s\n", (char*)vector_get(&v, i));
+//     }
 
-    vector_free(&v);
+//     vector_free(&v);
 
-    return 0;
-}
+//     return 0;
+// }
