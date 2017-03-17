3c92a0960ae16378b0dcb56797bb7d9286505fa1
Kelvin Zhang <k.zhang661@gmail.com>
2017-03-16 23:53:46 -0700

    refactor server

--- project2/packethandler.h
+++ project2/packethandler.h
@@ -4,6 +4,16 @@
 #define MAXPACKETSIZE 1024
 #define HEADERSIZE 20
 
+typedef struct Packet {
+    int ackNum;
+    int seqNum;
+    int retrans;
+    int syn;
+    int fin;
+    int len;
+    char *buf;
+} Packet;
+
 int sendPacket(int sockfd, struct sockaddr *dest_addr, socklen_t addrlen, Packet *pkt);
 
 int recvPacket(int sockfd, struct sockaddr *src_addr, socklen_t *addrlen, Packet *pkt);
